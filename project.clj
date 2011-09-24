6994dded5e099ca620d4529c75accc3c6b4b1b4f
nfma <nuno.filipe.marques@gmail.com>
2011-09-24 20:48:42 +0200

    missing ring-adapter

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [compojure "0.6.5" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
 		 [org.clojure.contrib/command-line "1.3.0-alpha4"]
+	         [ring/ring-jetty-adapter "0.3.11" :exclusions [javax.servlet/servlet-api ring/ring-core ring/ring-servlet org.clojure/clojure org.clojure/clojure-contrib]]
 		 [ring/ring-servlet "0.3.11"]
 		 [clj-http "0.2.1"]]
   :dev-dependencies [[lein-ring "0.4.5"]
-		     [ring/ring-jetty-adapter "0.3.11" :exclusions [javax.servlet/servlet-api ring/ring-core ring/ring-servlet org.clojure/clojure org.clojure/clojure-contrib]]
                      [ring/ring-devel "0.3.11" :exclusions [javax.servlet/servlet-api ring/ring-core ring/ring-servlet org.clojure/clojure org.clojure/clojure-contrib clj-stacktrace]]
                      [ring-reload-modified "0.1.1" :exclusions [javax.servlet/servlet-api ring/ring-core ring/ring-servlet org.clojure/clojure org.clojure/clojure-contrib]]]
   :ring {:handler social-github.core/app}
